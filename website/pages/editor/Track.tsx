53c8e96d05c115cd457998b4aa1e57fcc5437cae
Henrique Pessoa <titepessoa@gmail.com>
2022-12-12 10:00:46 -0300

    add note component

--- website/pages/editor/Track.tsx
+++ website/pages/editor/Track.tsx
@@ -1,39 +1,35 @@
 import React, { CSSProperties, DragEvent, FunctionComponent, HTMLAttributes, useState } from "react";
 import Bar from "../../entities/bar";
-import Note from "../../entities/note";
+import NoteEntity from "../../entities/note";
+import Note from "./Note";
 import { useSheet } from "./SheetContext";
 
 type TrackProps = {
   bar: Bar;
-  track: Note[];
-  handleAddNote: (note: Note) => void;
+  track: NoteEntity[];
+  handleAddNote: (note: NoteEntity) => void;
 };
 
 const Track: FunctionComponent<TrackProps> = ({ bar, track, handleAddNote }) => {
   const [isShowingPreview, setIsShowingPreview] = useState(false);
   const { sheet } = useSheet();
   const barSize = bar.beatCount / bar.dibobinador;
-  const remainingSizeInBar = barSize - Note.sumNotesDuration(track);
-  let previewHasOverflow = false;
+  const remainingSizeInBar = barSize - NoteEntity.sumNotesDuration(track);
   console.log(sheet);
 
-  const getNoteToAddSize = (note: Note | null) => {
-    if (note === null || !isShowingPreview) return 0;
+  const getNoteToAddSize = () => {
+    if (sheet.noteToAdd === null || !isShowingPreview) return 0;
 
-    let noteSize = note.duration;
+    let noteSize = sheet.noteToAdd.duration;
     if (noteSize > remainingSizeInBar) {
       noteSize = remainingSizeInBar;
-      previewHasOverflow = true;
+      sheet.noteToAdd.hasSustain = true;
     }
 
     return noteSize;
   };
 
   const getNoteSizePercentage = (noteSize: number) => `${(noteSize * 100) / barSize}%`;
-
-  const noteToAddSize = getNoteToAddSize(sheet.noteToAdd);
-  const noteToAddSizePercentage = getNoteSizePercentage(noteToAddSize);
-  const remainingSizeInBarPercentage = getNoteSizePercentage(remainingSizeInBar - noteToAddSize);
 
   const handleDragEnter = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
@@ -53,13 +49,6 @@ const Track: FunctionComponent<TrackProps> = ({ bar, track, handleAddNote }) => 
     handleAddNote(sheet.noteToAdd);
   };
 
-  const getNoteStyle = (note: Note): CSSProperties | undefined => {
-    if ((!note.isSustain && !note.hasSustain) || (note.isSustain && note.hasSustain)) return;
-
-    if (note.isSustain) return { width: "50%", display: "flex", justifyContent: "flex-start" };
-    if (note.hasSustain) return { width: "50%", display: "flex", justifyContent: "flex-end" };
-  };
-
   return (
     <div
       onDragEnter={event => handleDragEnter(event)}
@@ -69,62 +58,14 @@ const Track: FunctionComponent<TrackProps> = ({ bar, track, handleAddNote }) => 
       style={{ width: "100%", height: "100%", display: "flex" }}
     >
       {track.map((note, i) => (
-        <div
-          key={i}
-          style={{
-            border: "1px solid lightgray",
-            borderRadius: "4px",
-            width: getNoteSizePercentage(note.duration),
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {note.isSustain ? (
-            <div style={{ width: "50%", display: "flex", justifyContent: "flex-start" }}>
-              <span>...</span>
-            </div>
-          ) : null}
-          <div style={getNoteStyle(note)}>
-            <span>{note.pitch?.name}</span>
-          </div>
-          {note.hasSustain ? (
-            <div style={{ width: "50%", display: "flex", justifyContent: "flex-end" }}>
-              <span>...</span>
-            </div>
-          ) : null}
-        </div>
+        <Note key={i} note={note} style={{ width: getNoteSizePercentage(note.duration) }} />
       ))}
-      {isShowingPreview ? (
-        <div
-          style={{
-            border: "1px solid lightgray",
-            borderRadius: "4px",
-            width: noteToAddSizePercentage,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div
-            style={{
-              width: previewHasOverflow ? "50%" : "100%",
-              display: "flex",
-              justifyContent: previewHasOverflow ? "flex-end" : "center",
-            }}
-          >
-            <span>{sheet.noteToAdd?.pitch?.name}</span>
-          </div>
-          {previewHasOverflow ? (
-            <div style={{ width: "50%", display: "flex", justifyContent: "flex-end" }}>
-              <span>...</span>
-            </div>
-          ) : null}
-        </div>
+      {isShowingPreview && sheet.noteToAdd !== null ? (
+        <Note note={sheet.noteToAdd} style={{ width: getNoteSizePercentage(getNoteToAddSize()) }} />
       ) : null}
       <div
         style={{
-          width: remainingSizeInBarPercentage,
+          flexGrow: 1,
           margin: "auto 0px",
           height: "1px",
           border: "1px solid lightgray",
