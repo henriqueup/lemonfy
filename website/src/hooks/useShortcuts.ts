8f94442e2dbdd0491bf2aa53f8170b7b7dcecdb7
Henrique Pessoa <titepessoa@gmail.com>
2023-01-12 20:22:50 -0300

    create useShortcuts

---
+++ website/src/hooks/useShortcuts.ts
@@ -0,0 +1,60 @@
+import { useCallback, useEffect } from "react";
+
+const SHORTCUTS = {
+  CTRL_A: "bars.add",
+  CTRL_N: "notes.add",
+} as const;
+
+type ShortcutKey = keyof typeof SHORTCUTS;
+type ShortcutCode = (typeof SHORTCUTS)[ShortcutKey];
+
+const isShortcutKey = (key: string): key is ShortcutKey => SHORTCUTS.hasOwnProperty(key);
+
+type Shortcut = {
+  code: ShortcutCode;
+  key: ShortcutKey;
+  callback: () => void;
+};
+
+type ShortcutDictionary = {
+  [K in ShortcutKey]?: Shortcut;
+};
+
+const useShortcuts = (shortcutDictionary: ShortcutDictionary) => {
+  const getShortcutKey = (event: KeyboardEvent): ShortcutKey | undefined => {
+    const keyCodes: string[] = [];
+
+    if (event.ctrlKey) keyCodes.push("CTRL");
+    if (event.shiftKey) keyCodes.push("SHIFT");
+    if (event.altKey) keyCodes.push("ALT");
+
+    keyCodes.push(event.key);
+
+    const resultingKeyCode = keyCodes.join("_");
+    if (isShortcutKey(resultingKeyCode)) return resultingKeyCode;
+  };
+
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      const shortcutKey = getShortcutKey(event);
+      if (shortcutKey === undefined) return;
+
+      const shortcut = shortcutDictionary[shortcutKey];
+      if (shortcut === undefined) return;
+
+      event.preventDefault();
+      shortcut.callback();
+    },
+    [shortcutDictionary],
+  );
+
+  useEffect(() => {
+    document.addEventListener("keypress", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, [handleKeyPress]);
+};
+
+export { useShortcuts };
