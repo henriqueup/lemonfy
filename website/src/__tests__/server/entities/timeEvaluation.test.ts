e0ea5ceb69e4965842e762eb1ccd61ac74029219
Henrique Pessoa <titepessoa@gmail.com>
2023-02-05 12:30:09 -0300

    time evaluation tests

---
+++ website/src/__tests__/server/entities/timeEvaluation.test.ts
@@ -0,0 +1,28 @@
+import { TimeEvaluation } from "@entities/timeEvaluation";
+
+describe("Time Evaluation", () => {
+  const valueAboveTolerance = 1e-7;
+  const valueBelowTolerance = 1e-9;
+
+  it("Detectcs difference with value above tolerance", () => {
+    const valueA = 1;
+    const valueB = 1 + valueAboveTolerance;
+
+    expect(TimeEvaluation.IsEqualTo(valueA, valueB)).toBe(false);
+    expect(TimeEvaluation.IsGreaterThan(valueA, valueB)).toBe(false);
+    expect(TimeEvaluation.IsSmallerThan(valueA, valueB)).toBe(true);
+    expect(TimeEvaluation.IsGreaterOrEqualTo(valueA, valueB)).toBe(false);
+    expect(TimeEvaluation.IsSmallerOrEqualTo(valueA, valueB)).toBe(true);
+  });
+
+  it("Doesn't detect difference with value below tolerance", () => {
+    const valueA = 1;
+    const valueB = 1 + valueBelowTolerance;
+
+    expect(TimeEvaluation.IsEqualTo(valueA, valueB)).toBe(true);
+    expect(TimeEvaluation.IsGreaterThan(valueA, valueB)).toBe(false);
+    expect(TimeEvaluation.IsSmallerThan(valueA, valueB)).toBe(false);
+    expect(TimeEvaluation.IsGreaterOrEqualTo(valueA, valueB)).toBe(true);
+    expect(TimeEvaluation.IsSmallerOrEqualTo(valueA, valueB)).toBe(true);
+  });
+});
