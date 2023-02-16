817a25d8fd2a7412025a89fc5c3eb04a72385974
Henrique Pessoa <titepessoa@gmail.com>
2023-02-16 06:40:17 -0300

    add utils already mocked test

--- website/src/__tests__/mocks/utils.test.ts
+++ website/src/__tests__/mocks/utils.test.ts
@@ -25,4 +25,25 @@ describe("Mock entire modules", () => {
     const someMethodResultAfterMock = result.someMethod(3);
     expect(someMethodResultAfterMock).toBe(2);
   });
+
+  it("Skips already mocked methods", () => {
+    const someMethod = (a: number) => a;
+    const alreadyMocked = jest.fn();
+    const testModule = {
+      someConst: "abc",
+      otherConst: 23,
+      alreadyMocked,
+      someMethod,
+    };
+
+    const result = mockModuleFunctions(testModule);
+
+    expect(result.someConst).toBe("abc");
+    expect(result.otherConst).toBe(23);
+    expect(jest.isMockFunction(result.alreadyMocked)).toBe(true);
+    expect(jest.isMockFunction(result.someMethod)).toBe(true);
+
+    expect(result.alreadyMocked).toBe(alreadyMocked);
+    expect(result.someMethod).not.toBe(someMethod);
+  });
 });
