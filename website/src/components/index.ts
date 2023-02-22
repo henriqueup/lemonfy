0824972c5105f0738afe900d522982aafa7dfd97
Henrique Pessoa <titepessoa@gmail.com>
2023-02-22 07:18:03 -0300

    fix component modules

--- website/src/components/index.ts
+++ website/src/components/index.ts
@@ -1,5 +1,5 @@
-import Button from "./button";
+import { Button } from "./button";
 import { CollapsableSideMenu, FixedSideMenu } from "./sideMenu";
-import ClickAwayListener from "./clickAwayListener/ClickAwayListener";
+import { ClickAwayListener } from "./clickAwayListener";
 
 export { Button, ClickAwayListener, CollapsableSideMenu, FixedSideMenu };
