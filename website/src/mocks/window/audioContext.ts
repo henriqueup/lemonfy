6a2fcd60a2330b3a833d8b55dbc20913eb772752
Henrique Pessoa <titepessoa@gmail.com>
2023-03-17 06:48:18 -0300

    complete audiocontext mock

--- website/src/mocks/window/audioContext.ts
+++ website/src/mocks/window/audioContext.ts
@@ -1,15 +1,140 @@
-class AudioContext {
-  baseLatency!: number;
-  outputLatency!: number;
-  close!: () => Promise<void>;
-  createBufferSource!: () => AudioBufferSourceNode;
+interface BaseAudioContext extends EventTarget {
+  /** Available only in secure contexts. */
+  readonly audioWorklet: AudioWorklet;
+  readonly currentTime: number;
+  readonly destination: AudioDestinationNode;
+  readonly listener: AudioListener;
+  onstatechange: ((this: BaseAudioContext, ev: Event) => any) | null;
+  readonly sampleRate: number;
+  readonly state: AudioContextState;
+  createAnalyser(): AnalyserNode;
+  createBiquadFilter(): BiquadFilterNode;
+  createBuffer(numberOfChannels: number, length: number, sampleRate: number): AudioBuffer;
+  createBufferSource(): AudioBufferSourceNode;
+  createChannelMerger(numberOfInputs?: number): ChannelMergerNode;
+  createChannelSplitter(numberOfOutputs?: number): ChannelSplitterNode;
+  createConstantSource(): ConstantSourceNode;
+  createConvolver(): ConvolverNode;
+  createDelay(maxDelayTime?: number): DelayNode;
+  createDynamicsCompressor(): DynamicsCompressorNode;
+  createGain(): GainNode;
+  createIIRFilter(feedforward: number[], feedback: number[]): IIRFilterNode;
+  createOscillator(): OscillatorNode;
+  createPanner(): PannerNode;
+  createPeriodicWave(
+    real: number[] | Float32Array,
+    imag: number[] | Float32Array,
+    constraints?: PeriodicWaveConstraints,
+  ): PeriodicWave;
+  /** @deprecated */
+  createScriptProcessor(
+    bufferSize?: number,
+    numberOfInputChannels?: number,
+    numberOfOutputChannels?: number,
+  ): ScriptProcessorNode;
+  createStereoPanner(): StereoPannerNode;
+  createWaveShaper(): WaveShaperNode;
+  decodeAudioData(
+    audioData: ArrayBuffer,
+    successCallback?: DecodeSuccessCallback | null,
+    errorCallback?: DecodeErrorCallback | null,
+  ): Promise<AudioBuffer>;
+  addEventListener<K extends keyof BaseAudioContextEventMap>(
+    type: K,
+    listener: (this: BaseAudioContext, ev: BaseAudioContextEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  removeEventListener<K extends keyof BaseAudioContextEventMap>(
+    type: K,
+    listener: (this: BaseAudioContext, ev: BaseAudioContextEventMap[K]) => any,
+    options?: boolean | EventListenerOptions,
+  ): void;
+  removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions,
+  ): void;
+}
 
-  constructor(contextOptions?: AudioContextOptions | undefined) {
+/** An audio-processing graph built from audio modules linked together, each represented by an AudioNode. */
+interface AudioContext extends BaseAudioContext {
+  readonly baseLatency: number;
+  readonly outputLatency: number;
+  close(): Promise<void>;
+  createMediaElementSource(mediaElement: HTMLMediaElement): MediaElementAudioSourceNode;
+  createMediaStreamDestination(): MediaStreamAudioDestinationNode;
+  createMediaStreamSource(mediaStream: MediaStream): MediaStreamAudioSourceNode;
+  getOutputTimestamp(): AudioTimestamp;
+  resume(): Promise<void>;
+  suspend(): Promise<void>;
+  addEventListener<K extends keyof BaseAudioContextEventMap>(
+    type: K,
+    listener: (this: AudioContext, ev: BaseAudioContextEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions,
+  ): void;
+  removeEventListener<K extends keyof BaseAudioContextEventMap>(
+    type: K,
+    listener: (this: AudioContext, ev: BaseAudioContextEventMap[K]) => any,
+    options?: boolean | EventListenerOptions,
+  ): void;
+  removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions,
+  ): void;
+}
+
+class AudioContext implements AudioContext {
+  constructor() {
     return {
       baseLatency: 0,
       outputLatency: 0,
       close: jest.fn(),
       createBufferSource: jest.fn(),
+      createMediaStreamSource: jest.fn(),
+      createMediaElementSource: jest.fn(),
+      createMediaStreamDestination: jest.fn(),
+      getOutputTimestamp: jest.fn(),
+      resume: jest.fn(),
+      suspend: jest.fn(),
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+      audioWorklet: {} as AudioWorklet,
+      currentTime: 0,
+      destination: {} as AudioDestinationNode,
+      listener: {} as AudioListener,
+      onstatechange: jest.fn(),
+      sampleRate: 0,
+      state: "running",
+      createAnalyser: jest.fn(),
+      createBiquadFilter: jest.fn(),
+      createBuffer: jest.fn(),
+      createChannelMerger: jest.fn(),
+      createChannelSplitter: jest.fn(),
+      createConstantSource: jest.fn(),
+      createConvolver: jest.fn(),
+      createDelay: jest.fn(),
+      createDynamicsCompressor: jest.fn(),
+      createGain: jest.fn(),
+      createIIRFilter: jest.fn(),
+      createOscillator: jest.fn(),
+      createPanner: jest.fn(),
+      createPeriodicWave: jest.fn(),
+      createScriptProcessor: jest.fn(),
+      createStereoPanner: jest.fn(),
+      createWaveShaper: jest.fn(),
+      decodeAudioData: jest.fn(),
     };
   }
 }
