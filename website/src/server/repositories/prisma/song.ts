688807b0ec2589df8838087eb6133151cd90b608
Henrique Pessoa <titepessoa@gmail.com>
2023-05-03 21:05:58 -0300

    finish create mappings, bug with bars

--- website/src/server/repositories/prisma/song.ts
+++ website/src/server/repositories/prisma/song.ts
@@ -3,6 +3,8 @@ import type { Prisma, PrismaClient } from "@prisma/client";
 import type { ISongRepository } from "@domains/repositories";
 import type { Song } from "@entities/song";
 import type { Sheet } from "@entities/sheet";
+import type { Bar } from "@entities/bar";
+import type { Note } from "@entities/note";
 
 class SongPrismaRepository implements ISongRepository {
   readonly prisma!: PrismaClient;
@@ -13,6 +15,7 @@ class SongPrismaRepository implements ISongRepository {
 
   async create(song: Song) {
     console.log(`Create song '${song.name} - ${song.artist}' in prisma.`);
+    console.log(JSON.stringify(song));
 
     await this.prisma.song.create({
       data: mapSongToCreateInput(song),
@@ -24,15 +27,59 @@ const mapSongToCreateInput = (song: Song): Prisma.SongCreateInput => {
   return {
     name: song.name,
     artist: song.artist,
-    sheets: song.sheets.map(sheet => mapSheetToCreateInput(sheet)),
+    sheets: mapSheetsToCreateInput(song.sheets),
   };
 };
 
-const mapSheetToCreateInput = (
-  sheet: Sheet,
-): Prisma.SheetCreateWithoutSongInput => {
+const mapSheetsToCreateInput = (
+  sheets: Sheet[],
+): Prisma.SheetCreateNestedManyWithoutSongInput => {
   return {
-    trackCount: sheet.trackCount,
+    create: sheets.map(sheet => ({
+      trackCount: sheet.trackCount,
+      bars: mapBarsToCreateInput(sheet.bars),
+      tracks: mapTracksToCreateInput(sheet.tracks),
+    })),
+  };
+};
+
+const mapBarsToCreateInput = (
+  bars: Bar[],
+): Prisma.BarCreateNestedManyWithoutSheetInput => {
+  return {
+    create: bars.map(bar => ({
+      beatCount: bar.beatCount,
+      dibobinador: bar.dibobinador,
+      tempo: bar.tempo,
+      start: bar.start,
+      capacity: bar.capacity,
+      index: bar.index,
+    })),
+  };
+};
+
+const mapTracksToCreateInput = (
+  tracks: Note[][],
+): Prisma.TrackCreateNestedManyWithoutSheetInput => {
+  return {
+    create: tracks.map((track, i) => ({
+      index: i,
+      notes: mapNotesToCreateInput(track),
+    })),
+  };
+};
+
+const mapNotesToCreateInput = (
+  notes: Note[],
+): Prisma.NoteCreateNestedManyWithoutTrackInput => {
+  return {
+    create: notes.map(note => ({
+      pitch: note.pitch?.key,
+      start: note.start,
+      duration: note.duration,
+      hasSustain: note.hasSustain,
+      isSustain: note.isSustain,
+    })),
   };
 };
 
